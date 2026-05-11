20c85e5f555c961ae643c37ebad9cc8db56503fd
Ndugu2 <kalemakosea2@gmail.com>
2026-05-11 10:20:00 +0300

    refactor: modularize CSS and JS for improved performance and maintainability

---
+++ js/app.js
@@ -0,0 +1,13 @@
+﻿function searchResources() {
+    const input = document.getElementById('searchInput').value;
+    const resources = document.querySelectorAll('.resource');
+    
+    resources.forEach(res => {
+        const title = res.querySelector('h3').innerText;
+        if (title.indexOf(input) > -1) {
+            res.style.display = "";
+        } else {
+            res.style.display = "none";
+        }
+    });
+}
